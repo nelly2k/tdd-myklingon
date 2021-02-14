c89bda7c25bcbe951d135aa8c6c8324fbddc02de
Nelli Lovchikova (nul) <nul@amazon.com>
2021-02-14 17:43:35 +1100

    Make firt test pass

--- test/channel.test.ts
+++ test/channel.test.ts
@@ -1,5 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import '@aws-cdk/assert/jest';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+
+interface ChannelProps{
+    isReadOnly: boolean
+}
+
+class Channel extends cdk.Construct{
+    constructor(scope: cdk.Construct, id: string, props: ChannelProps) {
+        super(scope, id);
+        const api = new apigateway.RestApi(this, 'books-api');
+        api.root.addMethod('POST');
+      }
+}
 
 describe('Given that communication channel is needed', () => {
     describe('When changes are allowed', () => {
@@ -12,7 +25,7 @@ describe('Given that communication channel is needed', () => {
 
             expect(stack).toHaveResource("AWS::ApiGateway::Method", {
                 HttpMethod: 'POST'
-            })
+            });
         });
     });
 });
