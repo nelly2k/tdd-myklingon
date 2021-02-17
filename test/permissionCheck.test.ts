31b3ee8fd9dea5323e27bc2fa0ceb52cd76a340b
Nelli Lovchikova (nul) <nul@amazon.com>
2021-02-17 11:25:41 +1100

    Added custom matcher

--- test/permissionCheck.test.ts
+++ test/permissionCheck.test.ts
@@ -1,4 +1,6 @@
-
+import * as cdk from '@aws-cdk/core';
+import '@aws-cdk/assert/jest';
+import { SynthUtils } from '@aws-cdk/assert';
 
 describe('Given that scope is provided', () => {
     describe('When security policy is defined', () => {
@@ -16,7 +18,30 @@ describe('Given that scope is provided', () => {
 
             cdk.Aspects.of(stack).add(new PermissionCheck());
         
-            ???
+            
         });
     });
+});
+
+declare global {
+    namespace jest {
+      interface Matchers<R> {
+        toHaveError(message: string): R;
+      }
+    }
+}
+
+expect.extend({
+    toHaveWarning(received: cdk.Stack, message) {
+        const synthResult = SynthUtils.synthesize(received);
+
+        var pass = synthResult.messages
+            .filter(x => x.level == 'warning')
+            .some(x => x.entry.data == message);
+        const output = pass ? 'Template has expected error' : 'Temaplte doesn\'t have an expected error';
+
+        return {
+            pass, message: () => output
+          }
+    }
 });
